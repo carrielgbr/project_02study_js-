d6a351b01f10f6877974aa551da587d375baa99b
carrielgbr <gabrielcarriel61@gmail.com>
2025-06-01 18:32:41 -0300

    finalização de projeto

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 function adicionarTarefa() {
+      //mostra a mensagem de adicionado com sucesso, cada vez que clica no ok;
       let mensagem = "Tarefa adicionada com sucesso!";
-            //document.getElementById("mensagem").textContent = mensagem;
 
-
+      //procura no documento html o ID = "inputTarefa" e adiciona a informação na variavel do tipo texto "inputTarefa"
       let inputTarefa = document.getElementById("inputTarefa")
-      //console.log(inputTarefa)puxa a informação do html retornando todo o codigo html do input no console 
-      let tarefa = inputTarefa.value /* pega o valor do campo */
-      //console.log(tarefa) retorna no console do navegador
+       
+      // pega o valor da varivel "inputTarefa" e adiciona a varivel tarefa 
+      let tarefa = inputTarefa.value 
+      
+      //adiciona 
+      document.getElementById("mensagem").textContent = mensagem;
+      
+      let listaTarefas = document.getElementById("listaTarefas")
+      let novaTarefa = document.createElement("li")
 
-            document.getElementById("mensagem").textContent = tarefa;
+      novaTarefa.textContent = tarefa;
+
+
+      listaTarefas.appendChild(novaTarefa)
+
+      inputTarefa.value = ""
 }
-
